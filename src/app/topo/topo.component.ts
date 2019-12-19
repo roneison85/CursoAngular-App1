0c8b4c2c5bec69a097858efb5a04ddd6e9cdede4
Roneison M de Freitas <roneison.machado@gmail.com>
2019-12-19 10:07:45 -0300

    47. App1 - Painel (Template)

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,10 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-topo',
-    templateUrl: 'topo.component.html',
-    styleUrls: ['topo.component.css']
+    templateUrl: './topo.component.html',
+    styleUrls: ['./topo.component.css']
 })
-export class TopoComponent  {
-
-}
+export class TopoComponent  {}
