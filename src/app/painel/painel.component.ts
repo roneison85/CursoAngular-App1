10feb41afdfec29a80e4bdfc8c80a4e18df61f02
Roneison M de Freitas <roneison.machado@gmail.com>
2019-12-19 14:26:18 -0300

    57. App1 - Exibindo instrucao e frase (String interpolation)

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -10,6 +10,7 @@ import { FRASES } from './frases-mock';
 export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
+  public instrucao: string = 'Traduza a frase:';
 
   constructor() { console.log(this.frases) }
 
