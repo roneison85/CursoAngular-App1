8ed3ece7d304ed1e74d21f01497b9a97a2ecec6d
Roneison M de Freitas <roneison.machado@gmail.com>
2019-12-19 14:17:08 -0300

    56. App1 - Importando as frases para o componente Painel

--- src/app/painel/frases-mock.ts
+++ src/app/painel/frases-mock.ts
@@ -1,6 +1,6 @@
 import { Frase } from "../shared/frase.model";
 
-const FRASES: Frase[] = [
+export const FRASES: Frase[] = [
     {fraseEng: 'I like to learn', frasePtBR: 'Eu gosto de aprender'},
     {fraseEng: 'I watch tv', frasePtBR: 'Eu assisto tv'},
     {fraseEng: 'How are you?', frasePtBR: 'Como vai você?'},
