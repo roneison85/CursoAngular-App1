b972312d853e4bd96f5196a8c60506ec69b39c13
Roneison M de Freitas <roneison.machado@gmail.com>
2019-12-19 12:06:49 -0300

    55. Criando um Mock de objetos Frase

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from "../shared/frase.model";
+
+const FRASES: Frase[] = [
+    {fraseEng: 'I like to learn', frasePtBR: 'Eu gosto de aprender'},
+    {fraseEng: 'I watch tv', frasePtBR: 'Eu assisto tv'},
+    {fraseEng: 'How are you?', frasePtBR: 'Como vai você?'},
+    {fraseEng: 'I eat bread', frasePtBR: 'Eu como pão'}
+]
