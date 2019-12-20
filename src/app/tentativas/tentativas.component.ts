3f5a8dc54c12de6196749993557355ffc5d210a7
Roneison M de Freitas <roneison.machado@gmail.com>
2019-12-20 11:44:05 -0300

    68. App1 - Criando a classe Coracao

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Coracao } from '../shared/coracao.model';
+
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
@@ -9,6 +11,11 @@ export class TentativasComponent implements OnInit {
 
   public coracaoVazio: string = '/assets/coracao_vazio.png';
   public coracaoCheio: string = '/assets/coracao_cheio.png';
+
+  public coracoes: Coracao[] = [
+    new Coracao(true), new Coracao(true), new Coracao(true)
+  ];
+
   constructor() { }
 
   ngOnInit() {
